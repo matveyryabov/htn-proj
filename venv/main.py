c4afd7fa561eba0374af8724480e935feb023415
matveyryabov <matvey.a.ryabov@gmail.com>
2021-01-16 05:26:45 -0500

    Added basic camera support.

--- venv/main.py
+++ venv/main.py
@@ -1,12 +1,19 @@
 import random
+import time
 import kivy.app
 from kivy.app import App
+import logging
+from kivy.logger import Logger
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
+from kivy.uix.camera import Camera
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+
+#Logger.setLevel(logging.TRACE)
 
 class MenuScreen(Screen):
     pass
